6c3dfc29692956fecf24ee92879bfebf65b693ad
Ayeye11 <ayeye.devp@gmail.com>
2025-04-19 14:32:01 -0300

    feat(domain): add user profile enitity

--- src/domain/entities/user.entity.ts
+++ src/domain/entities/user.entity.ts
@@ -1,15 +1,49 @@
 interface UserIdentifier {
+  // Identifier
   id: string;
   email: string;
   password: string;
-}
 
-interface UserSecurity {
+  // Security
   phone: string;
   emailVerified: boolean;
   twoFactorAuth: boolean;
-  twoFactorSecret: string;
-  lastLogin: Date;
+  twoFactorSecret?: string;
+
+  // Logs
+  lastLogin?: Date;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt?: Date;
 }
 
-export type { UserIdentifier, UserSecurity };
+interface UserProfile {
+  // Identifier
+  id: string;
+
+  // Personal info
+  firstname: string;
+  lastname: string;
+  birthdate: string;
+  gender: "male" | "female";
+
+  // Identity
+  documentType: "DNI";
+  documentNumber: string;
+  nationality: string;
+
+  // Address
+  address: string;
+  postalCode: string;
+  province: string;
+  city: string;
+
+  // Wallets
+  wallets: string[];
+
+  // Experience
+  investmentExperience: "none" | "basic" | "intermediate" | "advanced";
+  riskTolerance: "low" | "medium" | "high";
+}
+
+export type { UserIdentifier, UserProfile };
