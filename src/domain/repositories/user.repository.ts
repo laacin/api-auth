6e89df9b1c1aa36599717e90c49a4d6e61ba494c
Ayeye11 <ayeye.devp@gmail.com>
2025-04-19 14:56:54 -0300

    feat(domain): add user profile methods on the user repo

--- src/domain/repositories/user.repository.ts
+++ src/domain/repositories/user.repository.ts
@@ -1,8 +1,11 @@
-import type { UserIdentifier } from "@domain/entities";
+import type { UserIdentifier, UserProfile } from "@domain/entities";
 
 export interface UserRepository {
   // Create
-  save(user: UserIdentifier): Promise<void>;
+  newUser(user: {
+    identifier: UserIdentifier;
+    profile: UserProfile;
+  }): Promise<void>;
 
   // Check
   isAvailable(email: string): Promise<boolean>;
@@ -13,6 +16,7 @@ export interface UserRepository {
   getUserById(id: string): Promise<UserIdentifier>;
   getUserByEmail(email: string): Promise<UserIdentifier>;
   getTwoFactorSecret(id: string): Promise<string>;
+  getProfile(id: string): Promise<UserProfile>;
 
   // Update
   changeEmail(id: string, email: string): Promise<void>;
@@ -20,6 +24,7 @@ export interface UserRepository {
   changePhone(id: string, phone: string): Promise<void>;
   saveTwoFactorSecret(id: string, secret: string): Promise<void>;
   verifyEmail(id: string): Promise<void>;
+  updateProfile(profile: UserProfile): Promise<void>;
 
   // Delete
   deleteAccount(id: string): Promise<void>;
